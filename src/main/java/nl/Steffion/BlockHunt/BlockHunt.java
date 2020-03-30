dc3b9e0563eec857f82d603a1842b900eedb9ba6
selfslaughter <self_slaughter@hotmail.com>
2020-03-30 16:36:05 +1000

    Removed LocationSerializable. Was only half implemented and broke old configs anyway

--- src/main/java/nl/Steffion/BlockHunt/BlockHunt.java
+++ src/main/java/nl/Steffion/BlockHunt/BlockHunt.java
@@ -39,7 +39,6 @@ import nl.Steffion.BlockHunt.Managers.CommandManager;
 import nl.Steffion.BlockHunt.Managers.ConfigManager;
 import nl.Steffion.BlockHunt.Managers.MessageManager;
 import nl.Steffion.BlockHunt.Managers.PermissionsManager;
-import nl.Steffion.BlockHunt.Serializables.LocationSerializable;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.WordUtils;
@@ -132,7 +131,6 @@ public class BlockHunt extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(new OnPlayerQuitEvent(), this);
 		getServer().getPluginManager().registerEvents(new OnSignChangeEvent(), this);
 
-		ConfigurationSerialization.registerClass(LocationSerializable.class, "BlockHuntLocation");
 		ConfigurationSerialization.registerClass(Arena.class, "BlockHuntArena");
 
 		pdfFile = getDescription();
